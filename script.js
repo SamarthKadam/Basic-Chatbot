6ee70339b36bdf1a1d2fd0d5dc631f250eb2e45f
Samarth Kadam <samarthskadam14@gmail.ocm>
2021-09-25 19:22:28 +0530

    First commmit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const inptext=document.getElementById('textmg');
 const sendbutton=document.querySelector('.png')
 const anime=document.querySelector('.msgloading');
 const contentsection=document.querySelector('.content');
-const userInfo=[];
+const userInfo=['94839338##','samarthskadam14@gmail.com','Samarth kadam','This is out of my scope','Thank you','Yes,Mr Chatbot '];
 let msginfo;
 console.log(userInfo);
-
+let count=0;
 
 ///This is initial message that happens first
 ///message is the class and theme of the bot ,user is the class of the usermessages and it has its theme
@@ -23,25 +23,74 @@ function Animation()
     }, 3000);
 }
 
-
-
+function Botcommands(msg){
+    count++;
+    setTimeout(function()
+    {
+        playAudio();
+    const html=`<div class="madi"><div class="message new${count}">${msg}</div></div>`///message
+    contentsection.insertAdjacentHTML('beforeend',html);
+    let desel=document.querySelector(`.new${count}`);;////////////////////Updated CODE
+    desel.scrollIntoView();
+},3000);
+}
 
 function addusersMsg(msg){
-    let message=`<div class="need"><div class="user">${msg}</div></div>`;
-    contentsection.insertAdjacentHTML('beforeend',message);
-    let x=0;
-    x++;
-    if(x===5)
+    count++;
+    if(msg==='')
     {
-    window.scrollTo(0,100);
+        alert("TEXTINPUT is emtpy please write something revelant");
+        return ;
     }
+    let message=`<div class="need"><div class="user new${count}">${msg}</div></div>`;
+    contentsection.insertAdjacentHTML('beforeend',message);
     Animation();
+    let desel=document.querySelector(`.new${count}`);////////////////////Updated CODE
+    desel.scrollIntoView();
+//   contentsection.scrollTo(0,desel.getBoundingClientRect().top ) ///Updated CODE
+    inptext.value='';
+
+    let str=msg;
+    let Arr=str.split(' ');
+    if(Arr.includes('phone')||Arr.includes('contact')||Arr.includes('number'))
+    {
+Botcommands('Here it is 9483938**');
+    }
+    else if(Arr.includes('hello')||Arr.includes('hi')||Arr.includes('who')||Arr.includes('Hello'))
+    {
+        Botcommands('Hi, this is Mr chatbot speaking');
+    }
+    else if(Arr.includes('email'))
+    {
+        Botcommands('Here samarthskadam14@');
+    }
+    else if(Arr.includes('thank')||Arr.includes('bye'))
+    {
+        Botcommands('Its my pleasure to have a talk with you');
+    }
+    else if(Arr.includes('full name')||Arr.includes('name'))
+    {
+        Botcommands('Samarth Kadam');
+    }
+    else{
+        Botcommands('Sorry, this is out of my reach');
+    }
+
 }
 
 
 function addHello(){
-    const html=`<div class="message">Hello, how may I help you ?</div>`///message
+    playAudio();
+    let html=`<div class="message new">Hello, how may I help you ?</div>`///message
     contentsection.insertAdjacentHTML('beforeend',html);
+    Animation();
+    setTimeout(function()
+    {
+        playAudio();
+    html=`<div class="message new">You can ask me about contact, email, and info of Samarth </div>`;
+    contentsection.insertAdjacentHTML('beforeend',html);
+    },3000);
+
 }
 
 const initial=setTimeout(function(){
@@ -57,13 +106,20 @@ setTimeout(function(){
 
 sendbutton.addEventListener('click',function(){
     msginfo=inptext.value;
-    addusersMsg(msginfo)
-
+    addusersMsg(msginfo);
     
 })
 
-
-
+document.addEventListener('keydown',function(e){
+    if(e.key==='Enter')
+    {
+    msginfo=inptext.value;
+    addusersMsg(msginfo);
+    }
+    else{
+        return;
+    }
+})
 
 
 
@@ -82,3 +138,9 @@ sendbutton.addEventListener('click',function(){
 ///1st step is to hover the element and message the  user about ANY STUFF   REquired?
 ///Bot should ask the user that =Hello ,How may I help you
 //So there are two way two types of text if bot text the color of the inserting adjacent html should be differnt color and user should be different and even about that small arrow
+
+// This is pop up song section
+var x = document.getElementById("myAudio");
+function playAudio() { 
+    x.play(); 
+  } 
